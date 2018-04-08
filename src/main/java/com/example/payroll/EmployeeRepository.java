ab260c8eafd3243fa0a9705f11829e3030a6a643
unknown <ryan_ahearn@comcast.net>
2018-04-08 11:05:06 -0400

    secured employees to their managers

--- src/main/java/com/example/payroll/EmployeeRepository.java
+++ src/main/java/com/example/payroll/EmployeeRepository.java
@@ -1,7 +1,20 @@
 package com.example.payroll;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
+@PreAuthorize("hasRole('ROLE_MANAGER')")
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
+    @Override
+    @PreAuthorize("#employee?.manager == null or #employee?.manager?.name == authentication?.name")
+    <S extends Employee> S save(@Param("employee") S employee);
+
+    @PreAuthorize("@employeeRepository.findOne(#id)?.manager?.name == authentication?.name")
+    void delete(@Param("id") Long id);
+
+    @Override
+    @PreAuthorize("#employee?.manager?.name == authentication?.name")
+    void delete(@Param("employee") Employee employee);
 }
